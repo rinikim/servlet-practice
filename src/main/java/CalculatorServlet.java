ac88955d3c93afeb238e6fb879b360fd7250c07f
rinikim <tytntu@naver.com>
2022-11-04 15:49:29 +0900

    HttpServlet 상속

--- src/main/java/CalculatorServlet.java
+++ src/main/java/CalculatorServlet.java
@@ -3,20 +3,23 @@ import calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 
 @WebServlet("/calculate")   // 해당 요청이 들어오면 실행해
-public class CalculatorServlet extends GenericServlet {
+public class CalculatorServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
     private ServletConfig servletConfig;
 
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("service");
         int operand1 = Integer.parseInt(request.getParameter("operand1"));
         String operator = request.getParameter("operator");
